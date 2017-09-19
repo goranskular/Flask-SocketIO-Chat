004e5c16ad08dc17f9e9a192941bec0253f4b62e
Goran Skular <goran.skular@slsolucije.hr>
2017-09-19 10:27:53 +0200

    heroku detect

--- chat.py
+++ chat.py
@@ -1,7 +1,15 @@
 #!/bin/env python
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
+if ON_HEROKU:
+    port = int(os.environ.get('PORT', 17995)) 
+else:
+    port = 3000
+
 from app import create_app, socketio
 
 app = create_app(debug=True)
 
 if __name__ == '__main__':
-    socketio.run(app,"0.0.0.0",80)
+    socketio.run(app,"0.0.0.0",port)
